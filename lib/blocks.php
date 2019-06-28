7c5bdf7a0b94f92fcdfae9d2739df010ffccfe94
Jorge <jorge.costa@developer.pt>
2019-06-28 21:01:34 +0100

    Add: Simple way to register block styles.

--- lib/blocks.php
+++ lib/blocks.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Block registration functions.
+ * Block and style registration functions.
  *
  * @package gutenberg
  */
@@ -45,3 +45,60 @@ function gutenberg_reregister_core_block_types() {
 	}
 }
 add_action( 'init', 'gutenberg_reregister_core_block_types' );
+
+/**
+ * Registers a new block style.
+ *
+ * @param string $block_name       Block type name including namespace.
+ * @param array  $style_properties Array containing the properties of the style name, label, style (name of the stylesheet to be enqueued), inline_style (string containing the CSS to be added).
+ */
+function register_block_style( $block_name, $style_properties ) {
+	if ( isset( $style_properties['style'] ) ) {
+		$style_handle = $style_properties['style'];
+		unset( $style_properties['style'] );
+		/** This action is documented in wp-admin/admin-footer.php */
+		// phpcs:ignore WordPress.NamingConventions.ValidHookName.UseUnderscores
+		add_action(
+			'enqueue_block_assets',
+			function() use ( $style_handle ) {
+				wp_enqueue_style( $style_handle );
+			},
+			30
+		);
+	}
+
+	if ( isset( $style_properties['inline_style'] ) ) {
+		$inline_style = $style_properties['inline_style'];
+		unset( $style_properties['inline_style'] );
+		add_action(
+			'enqueue_block_assets',
+			function() use ( $inline_style ) {
+				wp_add_inline_style( 'wp-block-library', $inline_style );
+			}
+		);
+	}
+
+	add_action(
+		'enqueue_block_assets',
+		function() use ( $block_name, $style_properties ) {
+			wp_add_inline_script(
+				'wp-blocks',
+				sprintf(
+					implode(
+						"\n",
+						array(
+							'( function() {',
+							'	wp.blocks.registerBlockStyle( \'%s\', %s );',
+							'} )();',
+						)
+					),
+					$block_name,
+					wp_json_encode( $style_properties )
+				),
+				'after'
+			);
+		}
+	);
+
+}
+
